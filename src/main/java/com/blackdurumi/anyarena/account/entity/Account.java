2f7ad82d606381c9c12e1c48b4b1ac94d504367e
jungh8n <jh981113@naver.com>
2024-03-15 02:13:43 +0900

    develop entity class of post domain

--- src/main/java/com/blackdurumi/anyarena/account/entity/Account.java
+++ src/main/java/com/blackdurumi/anyarena/account/entity/Account.java
@@ -1,10 +1,14 @@
 package com.blackdurumi.anyarena.account.entity;
 
+import com.blackdurumi.anyarena.post.entity.Comment;
+import com.blackdurumi.anyarena.post.entity.Post;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
@@ -40,4 +44,10 @@ public class Account extends BaseEntity {
 
     @Column(name = "phoneNumber")
     private String phoneNumber;
+
+    @OneToMany(mappedBy = "commenter")
+    private List<Comment> comments;
+
+    @OneToMany(mappedBy = "poster")
+    private List<Post> posts;
 }
---
+++ src/main/java/com/blackdurumi/anyarena/post/entity/Post.java
@@ -0,0 +1,62 @@
+package com.blackdurumi.anyarena.post.entity;
+
+import com.blackdurumi.anyarena.account.entity.Account;
+import com.blackdurumi.anyarena.account.entity.BaseEntity;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@NoArgsConstructor
+@Table(
+    name = "post"
+)
+@Builder
+@Getter
+@AllArgsConstructor
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Builder.Default
+    @ColumnDefault("0")
+    @Column(name = "views", nullable = false)
+    private Long views = 0L;
+
+    @Builder.Default
+    @ColumnDefault("0")
+    @Column(name = "likes", nullable = false)
+    private Long likes = 0L;
+
+    @Builder.Default
+    @ColumnDefault("0")
+    @Column(name = "unlikes", nullable = false)
+    private Long unlikes = 0L;
+
+    @ManyToOne()
+    @JoinColumn(name = "accountId")
+    private Account poster;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+}
